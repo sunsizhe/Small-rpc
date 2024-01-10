02698cc5930723ffc5b1a6c401d4505fd0be484a
sizhesun1998 <sun.siz@northeastern.edu>
2024-01-10 17:19:41 -0500

    client and server

---
+++ framework/src/main/java/com/sun/remoting/transport/netty/client/NettyRpcClient.java
@@ -0,0 +1,33 @@
+package com.sun.remoting.transport.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyRpcClient {
+
+    public static void main(String[] args) {
+        new NettyRpcClient().connect("10.0.0.248", 7397);
+    }
+
+    private void connect(String inetHost, int inetPort) {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.AUTO_READ, true);
+            b.handler(new MyChannelInitializer());
+            ChannelFuture f = b.connect(inetHost, inetPort).sync();
+            System.out.println("itstack-demo-netty client start done. {关注公众号：bugstack虫洞栈，获取源码}");
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
